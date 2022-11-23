043e5bbd114709665e49ec60a706b06251ecee10
mond1alaa111 <mond1alaa111@gmail.com>
2022-11-24 02:14:33 +0800

    update new freelancer page

--- pages/freelancerList.tsx
+++ pages/freelancerList.tsx
@@ -1,16 +1,73 @@
 import React from 'react'
-import { Box, Grid, TextField, styled} from '@mui/material'
+import { Box,Typography , Grid, TextField, styled} from '@mui/material'
 import Navbar from '../components/Navbar'
 import Listcard from '../components/Listcard'
 import Button from '@mui/material/Button';
 
+const grid2 = () =>{
+    <Grid item xs={8} md={3}>
+    <Listcard  service='UI/UX'
+description='we offer the best freelancers for interfce design and development'
+/>
+</Grid>
+}
 
 
 const freelancerList = ()=> {
     return(
         <>
         <Navbar />
-        
+        <Box sx={{padding:"10px"}}>
+        <Button sx={{padding:"10px 65px" ,
+        left:"84%"}} variant="contained" disableElevation>Add New List</Button>
+
+        <Box sx={{padding:"20px 10px",border:"1px solid black",margin:"30px 0px"}}>
+        <Typography variant="h3" gutterBottom>
+                On proggres projects
+        </Typography>
+            <Grid container spacing={3}>
+               
+                <Grid item xs={8} md={3}>
+                    <Listcard  service='UI/UX'
+                description='we offer the best freelancers for interfce design and development'
+                />
+                </Grid>
+                <Grid item xs={8} md={3}>
+                    <Listcard  service='UI/UX'
+                description='we offer the best freelancers for interfce design and development'
+                />
+                </Grid>
+                <Grid item xs={8} md={3}>
+                    <Listcard  service='UI/UX'
+                description='we offer the best freelancers for interfce design and development'
+                />
+                </Grid>
+                <Grid item xs={8} md={3}>
+                    <Listcard  service='UI/UX'
+                description='we offer the best freelancers for interfce design and development'
+                />
+                </Grid>
+                <Grid item xs={8} md={3}>
+                    <Listcard  service='UI/UX'
+                description='we offer the best freelancers for interfce design and development'
+                />
+                </Grid>
+                <Grid item xs={8} md={3}>
+                    <Listcard  service='UI/UX'
+                description='we offer the best freelancers for interfce design and development'
+                />
+                </Grid>
+            </Grid>
+
+       
+       
+
+        </Box>
+
+
+
+
+        </Box>
         
         
         </>
