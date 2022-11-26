fc580995cb71f2913ba6aa6f1a2a49d712ddcee0
mond1alaa111 <mond1alaa111@gmail.com>
2022-11-26 19:46:17 +0800

    update

--- components/Slideshow.tsx
+++ components/Slideshow.tsx
@@ -2,13 +2,14 @@ import Image from 'next/image'
 import React from 'react' 
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
+import {styles, TitleText} from '../components/StyledComponents'
 
 
 const Slideshow = () =>{
     return (
         <>
          <Splide aria-label="My Favorite Images">
-                <SplideSlide>
+                <SplideSlide style={{...styles.center}}>
                 <img src=" /img/le-buzz-studio-KiEiI2b9GkU-unsplash.jpg" alt="Image 1"/>
                   </SplideSlide>
                   <SplideSlide>
