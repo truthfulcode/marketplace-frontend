7e7b9ca1e01ae26a1910c09b0fb2c7df6190c586
hibbankh <hibbankamil@gmail.com>
2022-11-30 23:50:00 +0800

    ORDERS CONTACTUS

--- pages/contactUs.tsx
+++ pages/contactUs.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Box, TextField} from "@mui/material";
+import {Box, TextareaAutosize, TextField} from "@mui/material";
 import Navbar from '../components/Navbar';
 import {styles, SubmitButton, TitleText} from '../components/StyledComponents'
 import CustomForm from '../components/customForm';
@@ -8,15 +8,14 @@ const contactUs = () => {
   return (
         <Box sx={{...styles.header, ...styles.shadow,
                 backgroundImage: "url(/img/white-bg.png)",
-                height:"100vh",
-                backgroundRepeat:"no-repeat",
-                backgroundSize: "cover",}}>
+                height:"100vh"
+                }}>
             <Navbar signUp={false} signIn={false}/>
             <CustomForm>
             <TitleText>Comment and suggestions</TitleText>
                 <TextField placeholder='Name'/>
 		            <TextField placeholder='Email'/>
-                <TextField placeholder='Message'/>
+                <TextareaAutosize style={styles.formMessage} placeholder='Message'/>
                 <SubmitButton>SEND</SubmitButton>
               <TitleText>Our Contact</TitleText>
                 <SubmitButton>EMAIL SUPPORT</SubmitButton>
