52b3f7c9a5f9cf5d0e6f0dfbae0913485ed1593a
truthfulcode <imazziamine@graduate.utm.my>
2022-12-01 11:50:40 +0800

    minor fixes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import { MainButton } from './StyledComponents'
 
 const Navbar = ({signIn = true, signUp = true}) => {
   return (
-    <Box position="static" sx={{position:"absolute", marginBottom:"0", p:0,flexDirection:"row", display:"flex", alignItems:"center", backgroundColor:"transparent"}}>
+    <Box position="static" sx={{p:0,flexDirection:"row", display:"flex", alignItems:"center", backgroundColor:"transparent"}}>
         <Box sx={{flex:1}}>
             <Image src="/img/logo.png" alt="FreeWork logo" height={48} width={164}/>
         </Box>
--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home() {
         </TitleText>
         <Grid container spacing={2}>
           {["Web Design","Programming","Audio & Music","Business","Writing & Translation","Marketing",].map((value,index)=>(
-            <Grid item sx={{flexDirection:"column", ...styles.center }} key={`${value}+${index}`} xs={4}>
+            <Grid item sx={{flexDirection:"column", ...styles.center }} key={index} xs={4}>
               <Box sx={{border:"2px solid black",borderRadius:16, p:2, m:1}}>
                 <Image width={40} height={40} alt="category image" src={"/img/landing-categories/landingCategory"+(index).toString()+".png"}/>
               </Box>
