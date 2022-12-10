e7917eef626d4a0e8c826b02f15a895bfc882fe1
truthfulcode <imazziamine@graduate.utm.my>
2022-12-10 20:31:12 +0800

    update database schemas

--- utils/types.ts
+++ utils/types.ts
@@ -12,3 +12,8 @@ export enum PaymentType {
 export enum PaymentPage {
     Activity, Deposit, Withdraw, Setting 
 }
+export enum ProposalStatus {APPROVED,PENDING,REJECTED}
+export enum OrderStatus {SUCCESSFUL,PENDING,CANCELLED}
+export enum TransactionStatus {COMPLETED,PENDING,FAILED}
+export enum TransactionType {DEPOSIT,WITHDRAW,PAYMENT,REFUND}
+export enum TokenType {DAI,USDT,USDC}
