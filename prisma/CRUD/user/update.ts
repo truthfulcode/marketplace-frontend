6e9abd21a9603162bc9fba03fcdbd9214a75b380
hibbankh <hibbankamil@gmail.com>
2022-12-22 14:42:13 +0800

    backend for update (1 edit)

--- prisma/CRUD/user/update.ts
+++ prisma/CRUD/user/update.ts
@@ -1,15 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import express, { Request, Response } from 'express';
+import { MongoClient, Db } from 'mongodb';
 export default function handler(req:NextApiRequest, res:NextApiResponse){
     
 }
 
-////////////////////////////////////
-import express, { Request, Response } from 'express';
-import { MongoClient, Db } from 'mongodb';
-
 const app = express();
-
-// Replace with your MongoDB connection details
 const mongoUrl = 'mongodb://localhost:27017';
 const dbName = 'mydatabase';
 
@@ -26,15 +22,12 @@ MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true 
 
 app.put('/customer/:id', async (req: Request, res: Response) => {
   try {
-    // Validate the request body
     if (!req.body.email || !req.body.password) {
       return res.status(400).send({ error: 'Email and password are required' });
     }
 
-    // Update the customer account in the database
     const customer = await db.collection('customers').updateOne({ _id: req.params.id }, { $set: req.body });
 
-    // Return the updated customer in the response
     res.send(customer);
   } catch (error) {
     console.error(error);
@@ -44,10 +37,8 @@ app.put('/customer/:id', async (req: Request, res: Response) => {
 
 app.put('/freelancer/:id', async (req: Request, res: Response) => {
   try {
-    // Validate the request body
     if (!req.body.email || !req.body.password) {
       return res.status(400).send({ error: 'Email and password are required' });
     }
 
-    // Update the freelancer account in the database
     const freelancer = await
