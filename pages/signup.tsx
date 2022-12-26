dd66a258b8b2236200d58056b08474bef564e1ca
truthfulcode <imazziamine@graduate.utm.my>
2022-12-26 15:59:48 +0800

    update signup validation

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,5 +1,16 @@
 import React, { useState } from "react";
-import { Typography, Box, Grid, TextField, styled, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from "@mui/material";
+import {
+  Typography,
+  Box,
+  Grid,
+  TextField,
+  styled,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+} from "@mui/material";
 import Navbar from "../components/Navbar";
 import {
   styles,
@@ -8,7 +19,7 @@ import {
 } from "../components/StyledComponents";
 import FormWrapper from "../components/FormWrapper";
 import { Account, AccountType } from "@prisma/client";
-import { isString, sha512 } from "../utils/helpers";
+import { isString, onlyString, sha512, validEmail } from "../utils/helpers";
 import { login_keys } from "../utils/constants";
 import { FormInput, ValueWithError } from "../utils/types";
 import { useRouter } from "next/router";
@@ -18,14 +29,17 @@ const signup = () => {
   const router = useRouter();
   const createUser = async () => {
     let isFound = false;
+    console.log("MAP",account)
     login_keys.map((key) => {
       let val = account.get(key)?.value;
-      if (!val || val === null || val.length === 0) {
+      console.log("value",key, val);
+      if (!val || val.length === 0) {
         // throw an error
+        account.set(key,{value:account.get(key)?.value, error: "empty input"})
+        console.log("error",account.get(key)?.error)
         isFound = true;
       }
     });
-
     if (isFound) return;
     const _account: Account = {
       id: "",
@@ -56,15 +70,56 @@ const signup = () => {
     } catch (err) {}
   };
   
-  const setAccVal = (
+  const handleChange = (
     key: FormInput,
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
+    let isError = false;
+    let errorMessage;
+    let value = event.target.value;
+    switch (key) {
+      case "U": {
+        isError = value.length < 8;
+        errorMessage = "length must be greater or equal to 8";
+        break;
+      }
+      case "E": {
+        isError = validEmail(value)
+        errorMessage = "Invalid Email Format";
+        break;
+      }
+      case "F_N": {
+        isError = onlyString(value)
+        if(isError) errorMessage = "input should only be string"
+        break;
+      }
+      case "L_N": {
+        isError = onlyString(value)
+        if(isError) errorMessage = "input should only be string"
+        break;
+      }
+      case "P": {
+        isError = value.length < 18;
+        errorMessage = "length must be greater or equal to 18";
+        break;
+      }
+      case "P_N": {
+        isError = value.length !== 11;
+        errorMessage = "length not equal to 11"
+        break;
+      }
+      // case "U_T": {
+      //   isError = value.length < 18;
+      //   errorMessage = "length must be greater or equal to 18";
+      //   break;
+      // }
+    }
     let _account: Map<FormInput, ValueWithError> = account;
-    _account.set(key, {value : event.target.value, error: _account.get(key)?.error});
+    _account.set(key, { value: value, error: _account.get(key)?.error });
     setAccount(_account);
   };
-  const isValid = (key:FormInput) => typeof account.get(key)?.error === 'string'
+  const isValid = (key: FormInput) =>
+    typeof account.get(key)?.error === "string";
   return (
     <Box
       sx={{
@@ -81,58 +136,66 @@ const signup = () => {
         <TitleText>SIGN UP</TitleText>
         <TextField
           error={isValid("U")}
-          onChange={(e) => setAccVal("U", e)}
+          onChange={(e) => handleChange("U", e)}
           variant="outlined"
           placeholder="Username"
           helperText={account.get("U")?.error}
         />
         <TextField
           error={isValid("F_N")}
-          onChange={(e) => setAccVal("F_N", e)}
+          onChange={(e) => handleChange("F_N", e)}
           variant="outlined"
           placeholder="First Name"
           helperText={account.get("F_N")?.error}
         />
         <TextField
           error={isValid("L_N")}
-          onChange={(e) => setAccVal("L_N", e)}
+          onChange={(e) => handleChange("L_N", e)}
           variant="outlined"
           placeholder="Last Name"
           helperText={account.get("L_N")?.error}
         />
         <TextField
           error={isValid("P")}
-          onChange={(e) => setAccVal("P", e)}
+          onChange={(e) => handleChange("P", e)}
           variant="outlined"
           placeholder="Password"
           helperText={account.get("P")?.error}
         />
         <TextField
           error={isValid("E")}
-          onChange={(e) => setAccVal("E", e)}
+          onChange={(e) => handleChange("E", e)}
           variant="outlined"
           placeholder="Email"
           helperText={account.get("E")?.error}
         />
         <TextField
           error={isValid("P_N")}
-          onChange={(e) => setAccVal("P_N", e)}
+          onChange={(e) => handleChange("P_N", e)}
           variant="outlined"
           placeholder="Phone Nmber"
           helperText={account.get("P_N")?.error}
         />
-        <FormControl sx={{p:"14px"}} error={isValid("U_T")}>
+        <FormControl sx={{ p: "14px" }} error={isValid("U_T")}>
           <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
           <RadioGroup
             row
             defaultValue="CUSTOMER"
             aria-labelledby="demo-row-radio-buttons-group-label"
             name="row-radio-buttons-group"
-            onChange={(e) => setAccVal("U_T", e)}
+            onChange={(e) => handleChange("U_T", e)}
             aria-required
           >
-            <FormControlLabel value="CUSTOMER" control={<Radio />} label="Customer" />
-            <FormControlLabel value="FREELANCER" control={<Radio />} label="Freelancer" />
+            <FormControlLabel
+              value="CUSTOMER"
+              control={<Radio />}
+              label="Customer"
+            />
+            <FormControlLabel
+              value="FREELANCER"
+              control={<Radio />}
+              label="Freelancer"
+            />
           </RadioGroup>
         </FormControl>
         <SubmitButton onClick={createUser}>REGISTER</SubmitButton>
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,9 +1,9 @@
 import { FormInput } from "./types";
 
-export const login_keys : FormInput[] = ["USERNAME"
-  , "FIRST_NAME"
-  , "LAST_NAME"
-  , "EMAIL"
-  , "PASSWORD"
-  , "PHONE_NUMBER"
-  , "USER_TYPE"]
+export const login_keys : FormInput[] = ["U"
+  , "F_N"
+  , "L_N"
+  , "E"
+  , "P"
+  , "P_N"
+  , "U_T"]
