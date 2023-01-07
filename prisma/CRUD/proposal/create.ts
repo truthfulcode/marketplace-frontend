7c74d0d432b0669fc1e7f6c84392735fe620b84d
mond1alaa111 <mond1alaa111@gmail.com>
2023-01-08 01:11:58 +0800

    newUpdates

---
+++ prisma/CRUD/proposal/create.ts
@@ -0,0 +1,32 @@
+import { Proposal } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import {prisma} from "../../../utils/prisma"
+import { title } from 'process';
+
+
+
+export default async function createProposal(obj:Proposal) {
+    const {title,description,duration,status} = obj;
+    if (typeof title !== "string") throw Error('invalid username')
+    if (typeof description !== "string") throw Error('invalid username')
+
+    try{
+        await prisma.proposal.create({
+            data:{
+                proposal:{
+                    create:{
+                     title:title,
+                     description:description,
+                     duration:duration,
+                     status:status,
+                    }
+                }
+            }
+        })
+
+    }catch(err)
+    {
+        
+        }
+    }
+
