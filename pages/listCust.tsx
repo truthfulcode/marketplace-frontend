31992efd99bf0ad7ffc50d7db571d533d1aca3eb
hibbankh <hibbankamil@gmail.com>
2023-01-31 07:11:32 +0800

    test page

--- pages/listCust.tsx
+++ pages/listCust.tsx
@@ -1,80 +1,28 @@
 import React, { useState } from "react";
-import { Box, TextField, Grid, Typography } from "@mui/material";
+import {Box, Button, TextareaAutosize, TextField, Grid, Typography} from "@mui/material";
 import Navbar from '../components/Navbar';
-import { makeStyles } from '@material-ui/core/styles';
-import FormWrapper from "../components/FormWrapper";
-
-const useStyles = makeStyles({
-  header: {
-    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-    backgroundImage: "url(/img/white-bg.png)",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    height: "100vh"
-  },
-  orderNumberContainer: {
-    marginTop: "20px"
-  }
-});
+import Image from 'next/image'
+import {styles, SubmitButton, TitleText} from '../components/StyledComponents'
 
 const OrderListCustomer = () => {
-  const classes = useStyles();
-  const [orderNumber, setOrderNumber] = useState("");
-
-  const handleOrderNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setOrderNumber(event.target.value);
-  }
-
   return (
-    <Box className={classes.header}>
-      <Navbar signUp={false} signIn={false} />
-      <FormWrapper>
-        <Typography variant="h5" gutterBottom>
-          Check your order
-        </Typography>
-        <Grid container spacing={2} className={classes.orderNumberContainer}>
-          <Grid item xs={6}>
-            <TextField
-              fullWidth
-              label="Order Number"
-              value={orderNumber}
-              onChange={handleOrderNumberChange}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              fullWidth
-              type="date"
-              label="Order Date"
-              value={orderNumber}
-              onChange={handleOrderNumberChange}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="subtitle1" gutterBottom>
-              or
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label="Email"
-              value={orderNumber}
-              onChange={handleOrderNumberChange}
-            />
-          </Grid>
-        </Grid>
-        <Box mt={2}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            color="primary"
-            type="submit"
-            value="View Order"
-            onClick={() => alert(`Order number ${orderNumber}`)}
-          />
+    <Box sx={{ 
+      ...styles.header, 
+      ...styles.shadow,
+      minHeight:"100vh", height:"auto"}}> {/* Main Wrapper */}
+      <Navbar/>
+      <Box sx={{...styles.center, marginTop:"50px;"}}>
+        <TextField style={{width:"600px", outline:"100px", borderRadius:"100px", border:"none", marginRight:"0"}} placeholder="Search orders..."></TextField>
+        <Button style={{background:"#355C7D", marginLeft:"0px", height:"60px", borderRadius:"0", width:"80px", backgroundImage:'url("/img/search.png")', backgroundRepeat:"no-repeat", backgroundSize:"30px", backgroundPosition:"center"}}></Button>
+      </Box>
+      <Box sx={{...styles.center}}>
+        <Box sx={{display:"grid", gridTemplateColumns:"repeat( auto-fit, minmax(300px, 1fr) );", marginTop:"50px;", width:"50vw", minHeight:"600px", height:"auto", marginLeft:"0"}}>
+          <Box sx={{...styles.listingBox}}><a href="#">Order 1<Box sx={{...styles.center}}><TitleText style={{marginLeft:"10px", marginTop:"5px", fontSize: "20pt"}}>Order Tittle</TitleText></Box><p style={{textAlign:"center", marginTop:"-21px"}}>Lorem ipsum dolor, sit amet consectetur adipisicing.</p><Box sx={{...styles.center, marginBottom:"0", height:"50px", borderTop:"1px solid black"}}><TitleText style={{marginLeft:"5px", fontSize:"10pt", marginBottom:"-15px"}}>$45</TitleText></Box></a></Box>
+          <Box sx={{...styles.listingBox}}><a href="#">Order 2<Box sx={{...styles.center}}><TitleText style={{marginLeft:"10px", marginTop:"5px", fontSize: "20pt"}}>Order Tittle</TitleText></Box><p style={{textAlign:"center", marginTop:"-21px"}}>Lorem ipsum dolor, sit amet consectetur adipisicing.</p><Box sx={{...styles.center, marginBottom:"0", height:"50px", borderTop:"1px solid black"}}><TitleText style={{marginLeft:"5px", fontSize:"10pt", marginBottom:"-15px"}}>$45</TitleText></Box></a></Box>
+          <Box sx={{...styles.listingBox}}><a href="#">Order 3<Box sx={{...styles.center}}><TitleText style={{marginLeft:"10px", marginTop:"5px", fontSize: "20pt"}}>Order Tittle</TitleText></Box><p style={{textAlign:"center", marginTop:"-21px"}}>Lorem ipsum dolor, sit amet consectetur adipisicing.</p><Box sx={{...styles.center, marginBottom:"0", height:"50px", borderTop:"1px solid black"}}><TitleText style={{marginLeft:"5px", fontSize:"10pt", marginBottom:"-15px"}}>$45</TitleText></Box></a></Box>
+          <Box sx={{...styles.listingBox}}><a href="#">Order 4<Box sx={{...styles.center}}><TitleText style={{marginLeft:"10px", marginTop:"5px", fontSize: "20pt"}}>Order Tittle</TitleText></Box><p style={{textAlign:"center", marginTop:"-21px"}}>Lorem ipsum dolor, sit amet consectetur adipisicing.</p><Box sx={{...styles.center, marginBottom:"0", height:"50px", borderTop:"1px solid black"}}><TitleText style={{marginLeft:"5px", fontSize:"10pt", marginBottom:"-15px"}}>$45</TitleText></Box></a></Box>
         </Box>
-      </FormWrapper>
+      </Box>
     </Box>
   )
 }
