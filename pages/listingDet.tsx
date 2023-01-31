f703280923c5a3f2524e7e34434c5e0fd5b81daa
hibbankh <hibbankamil@gmail.com>
2023-01-31 09:03:21 +0800

    test

--- pages/listingDet.tsx
+++ pages/listingDet.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import {Box, Button, TextareaAutosize, TextField} from "@mui/material";
+import Navbar from '../components/Navbar';
+import Image from 'next/image'
+import {styles, SubmitButton, TitleText} from '../components/StyledComponents'
 
 const ViewListingDetails = () => {
   return (
--- pages/orderView.tsx
+++ pages/orderView.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import {Box, Button, TextareaAutosize, TextField, Grid, Typography} from "@mui/material";
+import Navbar from '../components/Navbar';
+import {styles, SubmitButton, TitleText} from '../components/StyledComponents'
 
 const OrderView = () => {
   return (
-    <div>orderView</div>
+    <Box sx={{ 
+      ...styles.header, 
+      ...styles.shadow,
+      minHeight:"100vh", height:"auto"}}> {/* Main Wrapper */}
+      <Navbar/>
+      <Box sx={{...styles.center, marginTop:"50px;"}}>
+        <TextField style={{width:"600px", outline:"100px", borderRadius:"100px", border:"none", marginRight:"0"}} placeholder="Search orders..."></TextField>
+        <Button style={{background:"#355C7D", marginLeft:"0px", height:"60px", borderRadius:"0", width:"80px", backgroundImage:'url("/img/search.png")', backgroundRepeat:"no-repeat", backgroundSize:"30px", backgroundPosition:"center"}}></Button>
+      </Box>
+      <Box sx={{...styles.center}}>
+          <Box sx={{...styles.listingBox, marginTop:"50px;"}}><a href="#">Order 1
+            <Box sx={{...styles.center}}><TitleText style={{marginLeft:"10px", marginTop:"5px", fontSize: "15pt"}}>Customer:</TitleText>
+            </Box><p style={{textAlign:"center", marginTop:"-21px"}}>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+            <Box sx={{...styles.center}}><TitleText style={{marginLeft:"10px", marginTop:"5px", fontSize: "15pt"}}>Order Tittle</TitleText>
+            </Box><p style={{textAlign:"center", marginTop:"-21px"}}>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+            <Box sx={{...styles.center}}><TitleText style={{marginLeft:"10px", marginTop:"5px", fontSize: "15pt"}}>Description</TitleText>
+            </Box><p style={{textAlign:"center", marginTop:"-21px"}}>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+            <Box sx={{...styles.center, marginBottom:"0", height:"50px", borderTop:"1px solid black"}}><TitleText style={{marginLeft:"5px", fontSize:"10pt", marginBottom:"-15px"}}>$45</TitleText>
+            </Box></a>
+          </Box>
+      </Box>
+    </Box>
   )
 }
 
